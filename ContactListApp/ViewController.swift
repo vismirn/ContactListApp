a9abfdf0a66746bb8111e658d94372ecd3e62b50
Viktor Smirnov <smirn04@yandex.ru>
2023-10-27 16:15:44 +0300

    Initial Commit

---
+++ ContactListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListApp
+//
+//  Created by Viktor Smirnov on 27.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
